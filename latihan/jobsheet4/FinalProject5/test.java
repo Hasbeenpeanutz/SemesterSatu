7f46d4533a21c6d9d1a4caade70dd5c04f11d288
peanutz <kazushi0890@gmail.com>
2025-10-02 11:50:37 +0700

    first commit

---
+++ latihan/jobsheet4/FinalProject5/test.java
@@ -0,0 +1,8 @@
+package latihan.jobsheet4.FinalProject5;
+
+public class test {
+    public static void main(String[] args) {
+        
+        System.out.println("Hi, this is the first row code");
+    }
+}
