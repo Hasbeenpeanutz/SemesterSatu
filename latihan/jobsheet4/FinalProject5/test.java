497620f55d8a44b53d55829d786243480ec218bd
peanutz <kazushi0890@gmail.com>
2025-10-06 20:43:31 +0700

    add folder latian

--- latihan/jobsheet4/FinalProject5/test.java
+++
@@ -1,8 +0,0 @@
-package latihan.jobsheet4.FinalProject5;
-
-public class test {
-    public static void main(String[] args) {
-        
-        System.out.println("Hi, this is the first row code");
-    }
-}
---
+++ latihan/jonsheet6/Selection2Trial105.java
@@ -0,0 +1,18 @@
+package jonsheet6;
+
+import java.util.Scanner;
+
+public class Selection2Trial105 {
+    public static void main(String[] args) {
+        Scanner input05 = new Scanner (System.in);
+
+        System.out.print("Enter year: ");
+        int year = input05.nextInt();
+
+        if ((year % 4) == 0 ) {
+            System.out.println("Leap Year");
+        } else{
+            System.out.println("Not a leap year");
+        }
+    }
+}
