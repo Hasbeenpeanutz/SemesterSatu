40add9c935ded99e653cdd561b49695abea1ccf6
peanutz <kazushi0890@gmail.com>
2025-10-31 20:39:18 +0700

    update jobsheet 5 & 6

--- jobsheet/jobsheet6/caseMethod.java
+++ jobsheet/jobsheet6/caseMethod.java
@@ -6,8 +6,8 @@ public class caseMethod {
     public static void main(String[] args) {
         Scanner ans = new Scanner(System.in);
 
-        double finalScore1, finalScore2, finalScore3, averageFinalScore;
-        String letterGrade1 = null, letterGrade2 = null, letterGrade3 = null, status1 = null, status2 = null, status3 = null, semesterStatus;
+        double finalScore1, finalScore2, averageFinalScore;
+        String letterGrade1 = null, letterGrade2 = null, status1 = null, status2 = null,  semesterStatus;
 
         System.out.println("==== INPUT STUDENT DATA ====");
         System.out.print("Name: ");
@@ -30,14 +30,6 @@ public class caseMethod {
         double finalExam2 = ans.nextDouble();
         System.out.print("Assignment Score: ");
         double Assignment2 = ans.nextDouble();
-
-        System.out.println("\n--- Course 3: Basic English---");
-        System.out.print("MidTerm Score: ");
-        double midTerm3 = ans.nextDouble();
-        System.out.print("FinalExam Score: ");
-        double finalExam3 = ans.nextDouble();
-        System.out.print("Assignment Score: ");
-        double Assignment3 = ans.nextDouble();
 
         finalScore1 = (midTerm1 * 0.3) + (finalExam1 * 0.4) + (Assignment1 * 0.3);
 
@@ -88,33 +80,8 @@ public class caseMethod {
             status2 = "NOT PASS";
         }
 
-        finalScore3 = (midTerm3 * 0.3) + (finalExam3 * 0.4) + (Assignment3 * 0.3);
-
-        if (finalScore3 >80 && finalScore3 <= 100) {
-            letterGrade3 = "A";
-            status3 = "PASS";
-        } else if (finalScore3 > 73 && finalScore3 <= 80) {
-            letterGrade1 = "B+";
-            status1 = "PASS";
-        } else if (finalScore3 > 65 && finalScore3 <= 73) {
-            letterGrade1 = "B";
-            status1 = "PASS";
-        } else if (finalScore3 > 60 && finalScore3 <= 65) {
-            letterGrade1 = "C+";
-            status1 = "PASS";
-        } else if (finalScore3 > 50 && finalScore3 <= 60) {
-            letterGrade1 = "C";
-            status1 = "NOT PASS";
-        } else if (finalScore3 > 39 && finalScore3 <= 50) {
-            letterGrade1 = "D";
-            status1 = "NOT PASS";
-        } else if (finalScore3 <= 39) {
-            letterGrade1 = "E";
-            status1 = "NOT PASS";
-        }
-
-        averageFinalScore = (finalScore1 + finalScore2 + finalScore3) / 3;
-        if (status1.equals("PASS") && status2.equals("PASS") && status3.equals("PASS")) {
+        averageFinalScore = (finalScore1 + finalScore2) / 2;
+        if (status1.equals("PASS") && status2.equals("PASS")) {
             if (averageFinalScore >= 70) {
                 semesterStatus = "PASS";
             } else {
@@ -131,8 +98,7 @@ public class caseMethod {
         System.out.println("----------------------------------------------------------------------------------------");
         System.out.printf("%-25s %-8.1f %-8.1f %-8.1f %-14.2f %-10s %-15s%n", "Algorithms Programming",  midTerm1,  finalExam1,  Assignment1, finalScore1, letterGrade1, status1);
         System.out.printf("%-25s %-8.1f %-8.1f %-8.1f %-14.2f %-10s %-15s%n", "Data Structure", midTerm2,  finalExam2,  Assignment2, finalScore2, letterGrade2, status2 + "\n");
-        System.out.printf("%-25s %-8.1f %-8.1f %-8.1f %-14.2f %-10s %-15s%n", "Basic English", midTerm3,  finalExam3,  Assignment3, finalScore3, letterGrade3, status3 + "\n");
-
+        
         System.out.println("Average Final Score: " + averageFinalScore);
         System.out.println("Semester Status: " + semesterStatus);
 
